46a8a4873c0c08f8734bad270a8730fa9e6b0758
jahnavi2706 <85930861+jahnavi2706@users.noreply.github.com>
2022-10-31 10:00:00 +0530

    Add files via upload

---
+++ cube of number.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int i,cube;
+    cout<<"enter a number : ";
+    cin>>i;
+    cube=i*i*i;
+    cout<<"cube of "<<i<<" is : "<<cube<<endl;
+    return 0;
+}
+
